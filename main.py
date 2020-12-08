9ec13f7804b68af0dbfb1f68935f975d03b0cd0a
nabil <merchant.nabil@gmail.com>
2020-12-08 10:31:22 -0600

    Finishing spirograph

--- main.py
+++ main.py
@@ -63,8 +63,16 @@ def random_color():
 
 
 tim.speed("fastest")
-tim.color(random_color())
-tim.circle(100)
+
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+
+draw_spirograph(5)
 
 screen = t.Screen()
 screen.exitonclick()
